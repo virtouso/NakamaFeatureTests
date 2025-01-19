f38d0373c51ea817007eb0f0e72e4cf9a0646743
DESKTOP-N0C205G\admin <s.moeen.kamali@gmail.com>
2025-01-19 19:00:02 +0330

    update

--- UnityClient/NakamaFeatures/FeaturesTest/Assets/Script/NakamaAutomaticMatch.cs
+++ UnityClient/NakamaFeatures/FeaturesTest/Assets/Script/NakamaAutomaticMatch.cs
@@ -8,29 +8,37 @@ using UnityEngine;
 
 public class NakamaAutomaticMatch : MonoBehaviour
 {
-    private string emailText = "";
-    private string inputText = "";
+    private string _emailText = "";
+    private string _inputText = "";
+    private string _rankText = "";
+    private string _healthText = "";
+    private string _regionText = "";
+    private string _rankRangeText = "";
+private string _healthRangeText = "";
+    
+    private const string Scheme = "http";
+    private const string Host = "116.203.192.124";
+    private const int Port = 7350;
+    private const string ServerKey = "defaultkey";
 
+    private IClient _client;
+    private ISocket _socket;
+    private ISession _session;
 
-    private const string scheme = "http";
-    private const string host = "116.203.192.124";
-    private const int port = 7350;
-    private const string serverKey = "defaultkey";
-
-    private IClient client;
-    private ISocket socket;
-    private ISession session;
-
-    private IMatch match;
-    private string matchId = "";
-    private string joinMatchId = "";
+    private IMatch _match;
+    private string _matchId = "";
+    private string _joinMatchId = "";
 
 
     void OnGUI()
     {
-        emailText = GUILayout.TextField(emailText, 25, GUILayout.Width(200));
-        inputText = GUILayout.TextField(inputText, 25, GUILayout.Width(200));
-
+        _emailText = GUILayout.TextField(_emailText, 25, GUILayout.Width(200));
+        _inputText = GUILayout.TextField(_inputText, 25, GUILayout.Width(200));
+        _rankText = GUILayout.TextField(_rankText, 25, GUILayout.Width(200));
+        _healthText = GUILayout.TextField(_healthText, 25, GUILayout.Width(200));
+        _regionText = GUILayout.TextField(_regionText, 25, GUILayout.Width(200));
+        _rankRangeText = GUILayout.TextField(_rankRangeText, 25, GUILayout.Width(200));
+        _healthRangeText= GUILayout.TextField(_healthRangeText, 25, GUILayout.Width(200));
         GUILayout.Space(10);
 
 
@@ -46,35 +54,34 @@ public class NakamaAutomaticMatch : MonoBehaviour
 
         if (GUILayout.Button("Send", GUILayout.Width(100)))
         {
-            SendMessage(inputText);
+            SendMessage(_inputText);
         }
 
         if (GUILayout.Button("Leave", GUILayout.Width(100)))
         {
-            socket.LeaveMatchAsync(match);
+            _socket.LeaveMatchAsync(_match);
         }
     }
 
     private async Task Authenticate()
     {
-        client = new Client(scheme, host, port, serverKey);
-        socket = Nakama.Socket.From(client);
+        _client = new Client(Scheme, Host, Port, ServerKey);
+        _socket = Nakama.Socket.From(_client);
 
         // Authenticate with device ID
-        session = await client.AuthenticateEmailAsync(emailText, "123456789");
-    
-        Debug.LogError($"Authenticated: {session.UserId}");
+        _session = await _client.AuthenticateEmailAsync(_emailText, "123456789");
+
+        Debug.LogError($"Authenticated: {_session.UserId}");
 
         // Connect socket
-        await socket.ConnectAsync(session);
+        await _socket.ConnectAsync(_session);
         Debug.LogError("Socket connected.");
 
         // Listen to match data
-        socket.ReceivedMatchState += OnReceivedMatchState;
-        
-        socket.ReceivedMatchmakerMatched += OnMatched;
-        socket.ReceivedMatchPresence += OnMatchPresence;
-      
+        _socket.ReceivedMatchState += OnReceivedMatchState;
+
+        _socket.ReceivedMatchmakerMatched += OnMatched;
+        _socket.ReceivedMatchPresence += OnMatchPresence;
     }
 
     private void OnMatchPresence(IMatchPresenceEvent message)
@@ -88,14 +95,14 @@ public class NakamaAutomaticMatch : MonoBehaviour
 
         foreach (var item in message.Leaves)
         {
-            Debug.LogError($"{item.Username} left the match {item.Persistence}  {item.Status}"); 
+            Debug.LogError($"{item.Username} left the match {item.Persistence}  {item.Status}");
         }
     }
 
     private async void OnMatched(IMatchmakerMatched newMatch)
     {
         Debug.LogError(newMatch.MatchId);
-        match = await socket.JoinMatchAsync(newMatch);
+        _match = await _socket.JoinMatchAsync(newMatch);
     }
 
 
@@ -108,32 +115,35 @@ public class NakamaAutomaticMatch : MonoBehaviour
 
     private async void SendMessage(string message)
     {
-        if (match != null)
+        if (_match != null)
         {
             byte[] data = System.Text.Encoding.UTF8.GetBytes(message);
-            await socket.SendMatchStateAsync(match.Id, 0, data);
+            await _socket.SendMatchStateAsync(_match.Id, 0, data);
             Debug.LogError($"Sent message: {message}");
         }
     }
 
     private async void CreateMatch()
     {
-        var ticket = await socket.AddMatchmakerAsync("*", 2, 2);
+        var numDic = new Dictionary<string, double>
+            { { HealthKey, int.Parse(_healthText) }, { RankKey, int.Parse(_rankText) } };
+        var stringDic = new Dictionary<string, string>() { { RegionKey, _regionText } };
+
+        var ticket = await _socket.AddMatchmakerAsync("*", 2, 2, stringDic, numDic);
 
         Debug.LogError($"ticket id:{ticket.Ticket}");
     }
 
-    // private async void JoinMatch(string inputMatchId)
-    // {
-    //     try
-    //     {
-    //         match = await socket.JoinMatchAsync(inputMatchId);
-    //         matchId = match.Id;
-    //         Debug.LogError($"Joined match with ID: {inputMatchId}");
-    //     }
-    //     catch (Exception e)
-    //     {
-    //         Debug.LogError($"Failed to join match: {e.Message}");
-    //     }
-    // }
+
+    private string CreateQuery()
+    {
+        return null;
+    }
+    
+    private static List<string> _regions = new() { "asia", "eu", "africa" };
+    private const string HealthKey = "health";
+    private const string RankKey = "rank";
+    private const string RegionKey = "region";
+
+
 }
